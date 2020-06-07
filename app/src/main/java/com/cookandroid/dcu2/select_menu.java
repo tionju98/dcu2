137c5dea397ae52ca94397c4f798b8cf6f31d2bd
tionju98 <tlsguswn0417@naver.com>
2020-06-07 20:38:36 +0900

    dcu2

--- app/src/main/java/com/cookandroid/dcu2/select_menu.java
+++ app/src/main/java/com/cookandroid/dcu2/select_menu.java
@@ -6,13 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class select_menu extends AppCompatActivity {
+
+    TextView foodname_1_1,foodprice_1_1;
+
+    String choice = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.select_menu);
+        this.InitializeView();
+        this.setTextView();
 
 
         //주문하기 버튼 누르면 결제 창으로 넘어가기
@@ -25,5 +32,14 @@ public class select_menu extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+    public void InitializeView(){
+        foodname_1_1=(TextView)findViewById(R.id.foodname_1_1);
+        foodprice_1_1=(TextView)findViewById(R.id.foodprice_1_1);
+    }
+    public void setTextView(){
+        foodname_1_1.setText(getString(R.string.foodname_1_1));
+        foodname_1_1.setText(getString(R.string.foodprice_1_1));
+
     }
 }
